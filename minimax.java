04611efe9c42816cf0236d52146b1280bf49c340
Gavin Grey <gjgrey@ucdavis.edu>
2020-02-10 12:11:43 -0800

    Initial evaluation function

--- minimax.java
+++ minimax.java
@@ -62,10 +62,151 @@ public class minimax extends AIModule
         }
     }
 
+    private void numInARow(final GameStateModule state, int inARow[])
+    {
+        int x = state.getWidth();
+        int y = state.getHeight();
+        parseHorizontal(state, inARow, x, y);
+        parseVertical(state, inARow, x, y);
+        parseDiagonal(state, inARow, x, y);
+    }
+
+    private void parseHorizontal(final GameStateModule state, int inARow[], int width, int height)
+    {
+        for(int y = 0; y < height; y++)
+        {
+            for(int x = 0; x < width; x++)
+            {
+                if(state.getAt(x, y) == player)
+                {
+                    int consec = 0;
+                    while(x < width)
+                    {
+                        if(state.getAt(x, y) == player)
+                        {
+                            consec++;
+                            x++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if(consec >= 2)
+                    {
+                        if(consec > 4)
+                        {
+                            consec = 4;
+                        }
+                        inARow[consec - 2]++;
+                    }
+                }
+            }
+        }
+    }
+
+    private void parseVertical(final GameStateModule state, int inARow[], int width, int height)
+    {
+        for(int x = 0; x < width; x++)
+        {
+            for(int y = 0; y < width; y++)
+            {
+                if(state.getAt(x, y) == player)
+                {
+                    int consec = 0;
+                    while(y < height)
+                    {
+                        if(state.getAt(x, y) == player)
+                        {
+                            consec++;
+                            y++;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                    if(consec >= 2)
+                    {
+                        if(consec > 4)
+                        {
+                            consec = 4;
+                        }
+                        inARow[consec - 2]++;
+                    }
+                }
+            }
+        }
+    }
+
+    private void parseDiagonal(final GameStateModule state, int inARow[], int width, int height)
+    {
+        //First half
+        for(int y = 0; y < height; y++)
+        {
+            for(int diag = 0; y+diag < height && diag < width; diag++)
+            {
+                if(state.getAt(diag, y + diag) != player)
+                {
+                    continue;
+                }
+                int consec = 0;
+                while(y+diag < height && diag < width)
+                {
+                    if(state.getAt(diag, y + diag) != player)
+                    {
+                        break;
+                    }
+                    consec++;
+                    diag++;
+                }
+                if(consec >= 2)
+                {
+                    if(consec > 4)
+                    {
+                        consec = 4;
+                    }
+                    inARow[consec - 2]++;
+                }
+            }
+        }
+
+        //Second half
+        for(int x = 1; x < width; x++)
+        {
+            for(int diag = 0; x+diag < width && diag < height; diag++)
+            {
+                if(state.getAt(x + diag, diag) != player)
+                {
+                    continue;
+                }
+                int consec = 0;
+                while(x+diag < width && diag < height)
+                {
+                    if(state.getAt(x + diag, diag) != player)
+                    {
+                        break;
+                    }
+                    consec++;
+                    diag++;
+                }
+                if(consec >= 2)
+                {
+                    if(consec > 4)
+                    {
+                        consec = 4;
+                    }
+                    inARow[consec - 2]++;
+                }
+            }
+        }
+    }
+
     // randomly assigns a value to a state
 	private int eval(final GameStateModule state){
-	    Random rand = new Random();
-		return rand.nextInt(100);
+        int inARow[] = {0, 0, 0};
+        numInARow(state, inARow);
+        return(10 * inARow[0] + 100 * inARow[1] + 1000 * inARow[2]);
 	}
 
 
